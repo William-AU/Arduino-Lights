9f5ad3ac39c467ca87f8ee4ca10c7470bd693465
au682687 <202006324@post.au.dk>
2022-06-01 15:33:06 +0200

    Added error handling

--- src/main/java/app/serial/CommandQueue.java
+++ src/main/java/app/serial/CommandQueue.java
@@ -37,10 +37,38 @@ public class CommandQueue {
             serialPort.getOutputStream().write(command.getBytes(StandardCharsets.UTF_8));
             serialPort.getOutputStream().flush();
             int ack = serialPort.getInputStream().read();
+            if (ack != 0xC8) {
+                printError();
+            }
             return ack;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return -1;
+    }
+
+    private void printError() {
+        try {
+            byte errorCode = (byte) serialPort.getInputStream().read();
+            switch (errorCode) {
+                case 0x00 -> System.out.println("ERROR: Command timed out");
+                case 0x01 -> {
+                    int expectedLength = serialPort.getInputStream().read();
+                    int actualLength = serialPort.getInputStream().read();
+                    System.out.println("ERROR: Unexpected command size, received length: " + actualLength
+                            + "expected length: " + expectedLength);
+                }
+                case 0x02 -> System.out.println("ERROR: Missing command terminator");
+                case 0x03 -> {
+                    int code = serialPort.getInputStream().read();
+                    System.out.println("ERROR: Unexpected command opcode, received: " + code);
+                }
+                case 0x04 -> System.out.println("ERROR: Incorrect number of arguments");
+                case (byte) 0xFF -> System.out.println("ERROR: An unexpected error occurred");
+                default -> System.out.println("ERROR: Unexpected error message signature received");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
