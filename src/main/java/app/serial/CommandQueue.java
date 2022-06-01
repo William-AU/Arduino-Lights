8aebd595c32f74817f259bea485867e2ec2baadd
au682687 <202006324@post.au.dk>
2022-06-01 15:54:35 +0200

    Added support for clumping commands together in chunks

--- src/main/java/app/serial/CommandQueue.java
+++ src/main/java/app/serial/CommandQueue.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 @Component
 public class CommandQueue {
@@ -27,14 +26,23 @@ public class CommandQueue {
     @Scheduled(fixedDelay = 1)
     private void send() {
         if (!queue.isEmpty()) {
-
+            StringBuilder command = new StringBuilder(queue.remove());
+            if (command.toString().toLowerCase().contains("setled")) {
+                while (queue.peek() != null && queue.peek().toLowerCase().contains("setled")) {
+                    command.append(";").append(queue.remove());
+                }
+            }
+            for (byte[] bytes : clumpCommands(command.toString())) {
+                int ack = send(bytes);
+                if (ack != 0xC8) break;
+            }
         }
     }
 
     // TODO: add a timeout somewhere here
-    private int send(String command) {
+    private int send(byte[] command) {
         try {
-            serialPort.getOutputStream().write(command.getBytes(StandardCharsets.UTF_8));
+            serialPort.getOutputStream().write(command);
             serialPort.getOutputStream().flush();
             int ack = serialPort.getInputStream().read();
             if (ack != 0xC8) {
@@ -45,6 +53,37 @@ public class CommandQueue {
             e.printStackTrace();
         }
         return -1;
+    }
+
+    // This method is pretty dumb and slow, but this will basically never be the bottleneck, so it doesn't actually matter
+    private List<byte[]> clumpCommands(String command) {
+        // The idea here is to create chunks of commands that are as close to the serial buffer size as possible
+        String[] commandArr = command.split(";");
+        StringBuilder currentGuess = new StringBuilder();
+        List<byte[]> res = new ArrayList<>();
+        int currentLength = 0;
+        for (int i = 0; i < commandArr.length; i++) {
+            int newLength;
+            if (currentGuess.isEmpty()) {
+                newLength = currentLength + BytecodeConverter.convertToBytes(commandArr[i]).length;
+            } else {
+                newLength = currentLength + BytecodeConverter.convertToBytes(currentGuess + ";" + commandArr[i]).length;
+            }
+            // Safety range of 10
+            if (newLength > (SerialConstants.BUFFER_SIZE - 10)) {
+                res.add(BytecodeConverter.convertToBytes(currentGuess + ";" + commandArr[i]));
+                currentLength = 0;
+                currentGuess = new StringBuilder();
+            } else {
+                if (currentGuess.isEmpty()) {
+                    currentGuess.append(commandArr[i]);
+                } else {
+                    currentGuess.append(";").append(commandArr[i]);
+                }
+                currentLength = newLength;
+            }
+        }
+        return res;
     }
 
     private void printError() {
